a331ef84ff5757f900abe6dc6127522b47eae782
William White <63733033+SirLongsword@users.noreply.github.com>
2025-10-14 20:11:28 +0200

    rewards basics

---
+++ TRAFFIK_APP/Models/Entities/Reward/RewardItem.cs
@@ -0,0 +1,7 @@
+﻿public class RewardItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Cost { get; set; } // Points required
+}
--- TRAFFIK_APP/ViewModels/DashboardViewModel.cs
+++ TRAFFIK_APP/ViewModels/DashboardViewModel.cs
@@ -9,6 +9,7 @@ namespace TRAFFIK_APP.ViewModels
     {
         private readonly BookingClient _bookingClient;
         private readonly RewardClient _rewardClient;
+        private readonly RewardCatalogClient _catalogClient;
         private readonly NotificationClient _notificationClient;
         private readonly VehicleClient _vehicleClient;
         private readonly SessionService _session;
@@ -16,6 +17,11 @@ namespace TRAFFIK_APP.ViewModels
         public ObservableCollection<Booking> Bookings { get; } = new();
         public ObservableCollection<Notification> Notifications { get; } = new();
         public ObservableCollection<Vehicle> Vehicles { get; } = new();
+        public ObservableCollection<RewardItemDto> AvailableRewards { get; } = new();
+        public ObservableCollection<RewardItemDto> LockedRewards { get; } = new();
+
+        public IEnumerable<RewardItemDto> TopAvailableRewards => AvailableRewards.Take(3);
+        public IEnumerable<RewardItemDto> FourthLockedReward => LockedRewards.Take(1);
 
         public int RewardBalance { get; set; }
         public string UserFullName => _session.UserName;
@@ -29,12 +35,14 @@ namespace TRAFFIK_APP.ViewModels
         public DashboardViewModel(
             BookingClient bookingClient,
             RewardClient rewardClient,
+            RewardCatalogClient catalogClient,
             NotificationClient notificationClient,
             VehicleClient vehicleClient,
             SessionService session)
         {
             _bookingClient = bookingClient;
             _rewardClient = rewardClient;
+            _catalogClient = catalogClient;
             _notificationClient = notificationClient;
             _vehicleClient = vehicleClient;
             _session = session;
@@ -58,19 +66,22 @@ namespace TRAFFIK_APP.ViewModels
             Bookings.Clear();
             Notifications.Clear();
             Vehicles.Clear();
+            AvailableRewards.Clear();
+            LockedRewards.Clear();
 
             var bookings = await _bookingClient.GetByUserAsync(userId);
             var notifications = await _notificationClient.GetAllAsync();
             var balance = await _rewardClient.GetBalanceAsync(userId);
             var vehicleDtos = await _vehicleClient.GetByUserAsync(userId);
+            var catalog = await _catalogClient.GetAllAsync();
+
+            RewardBalance = balance ?? 0;
 
             if (bookings is not null)
                 foreach (var b in bookings) Bookings.Add(b);
 
             if (notifications is not null)
                 foreach (var n in notifications) Notifications.Add(n);
-
-            RewardBalance = balance ?? 0;
 
             if (vehicleDtos is not null)
             {
@@ -86,6 +97,21 @@ namespace TRAFFIK_APP.ViewModels
 
                 foreach (var v in vehicles) Vehicles.Add(v);
             }
+
+            if (catalog is not null)
+            {
+                foreach (var reward in catalog)
+                {
+                    if (reward.Cost <= RewardBalance)
+                        AvailableRewards.Add(reward);
+                    else
+                        LockedRewards.Add(reward);
+                }
+            }
+
+            OnPropertyChanged(nameof(RewardBalance));
+            OnPropertyChanged(nameof(TopAvailableRewards));
+            OnPropertyChanged(nameof(FourthLockedReward));
         }
     }
 }
