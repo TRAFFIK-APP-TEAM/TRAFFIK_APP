43afa001b52953055eba7d828fcba292c4244ae5
ConnorFreddles <155996427+ConnorFreddles@users.noreply.github.com>
2025-10-16 00:18:07 +0200

    StaffDash

---
+++ TRAFFIK_APP/ViewModels/StaffDashboardViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TRAFFIK_APP.Models;
+
+
+namespace TRAFFIK_APP.ViewModels
+{
+    public class StaffDashboardViewModel : BaseViewModel
+    {
+        public ObservableCollection<BookingStageUpdateDto> ActiveBookings { get; set; } = new();
+
+        public ICommand UpdateStageCommand { get; }
+
+        public StaffDashboardViewModel()
+        {
+            UpdateStageCommand = new Command<BookingStageUpdateDto>((booking) =>
+            {
+                //API TO BE PLUGGED IN HERE 
+            });
+        }
+    }
+
+   
+}
