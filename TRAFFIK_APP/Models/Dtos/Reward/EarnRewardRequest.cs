ef42a909ac5a2867c2053808d93b2fc9ba862a4b
William White <63733033+SirLongsword@users.noreply.github.com>
2025-10-22 15:04:44 +0200

    polishing rewards logic

--- TRAFFIK_APP/Models/Dtos/Reward/EarnRewardRequest.cs
+++ TRAFFIK_APP/Models/Dtos/Reward/EarnRewardRequest.cs
@@ -1,10 +1,8 @@
-﻿namespace TRAFFIK_APP.Models.Dtos.Reward
-{
+﻿namespace TRAFFIK_APP.Models.Dtos.Reward {
     public class EarnRewardRequest
     {
         public int UserId { get; set; }
         public int BookingId { get; set; }
-        public int Points { get; set; }
-        public string Description { get; set; } = string.Empty;
+        public decimal AmountSpent { get; set; }
     }
 }
