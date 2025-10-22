6c5556f454fbfb74db3f77322bbf5fb328b670df
William White <63733033+SirLongsword@users.noreply.github.com>
2025-10-22 23:41:09 +0200

    UI improvements and such

--- TRAFFIK_APP/Views/RewardsPage.xaml.cs
+++ TRAFFIK_APP/Views/RewardsPage.xaml.cs
@@ -4,9 +4,18 @@ namespace TRAFFIK_APP.Views;
 
 public partial class RewardsPage : ContentPage
 {
+    private RewardsViewModel ViewModel => BindingContext as RewardsViewModel;
+
     public RewardsPage(RewardsViewModel viewModel)
     {
         InitializeComponent();
         BindingContext = viewModel;
+        Loaded += OnPageLoaded;
+    }
+
+    private async void OnPageLoaded(object sender, EventArgs e)
+    {
+        if (ViewModel?.RefreshCommand?.CanExecute(null) == true)
+            ViewModel.RefreshCommand.Execute(null);
     }
 }
